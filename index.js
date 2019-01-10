ad662c65988150ec879ce268afb7627d7def6b8b
adriancalle173 <adriancalle173@gmail.com>
2019-01-09 22:40:52 -0600

    First test for the solution

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const answers = require('./answers.js');
 const puzzle = require('./puzzle.js');
 
 app.get('/', function (req, res) {
-  console.log(req.query);
+
   if(req.query.q === 'Puzzle'){
     answers['Puzzle'] = puzzle.solve(req.query.d);
   }
   let response = answers[req.query.q];
   res.send(response);
+  
 });
 
 app.listen(process.env.PORT || 8080, function () {
--- puzzle.js
+++ puzzle.js
@@ -1,33 +1,40 @@
 module.exports = {
     solve: function(input){
 
-        input = "Please solve this puzzle:\n ABCD\nA-->-\nB-=--\nC->--\nD-<--\n";
         // 1. Get weight of the letters
         let inputSplit = input.split('\n');
         let lettersWeight = this.getLettersWeight(inputSplit);
 
         // 2. Apply letters weights to fill the matrix and give a solution
         let solution = this.fillMatrix(lettersWeight);
-
+        console.log(solution);
         return solution;
 
     },
     getLettersWeight: function(inputSplit){
 
-        let lettersWeight = {
-            '0': 0,
-            '1': 0,
-            '2': 0,
-            '3': 0
-        }
+        let lettersWeight = [0, 0, 0, 0];
 
         inputSplit.forEach(function(rule, index){
             if(index>1){
                 var ruleSplit = rule.split('');
                 ruleSplit.shift(); // Remove letter from the rule
                 ruleSplit.forEach(function(operator, position){
-                    if(operator !== '-'){
-                        // TODO
+                    if(operator === '>'){
+                        for(j=0; j < lettersWeight.length; j++){
+                            if(lettersWeight[j] > lettersWeight[index - 2]){
+                                lettersWeight[j] += 1;
+                            }
+                        }
+                        lettersWeight[index - 2] = lettersWeight[position] + 1;
+                        
+                    } else if (operator === '<'){
+                        for(j=0; j < lettersWeight.length; j++){
+                            if(lettersWeight[j] < lettersWeight[index - 2]){
+                                lettersWeight[j] -= 1;
+                            }
+                        }
+                        lettersWeight[index - 2] = lettersWeight[position] - 1;
                     }
                 })
             }
@@ -35,7 +42,7 @@ module.exports = {
         return lettersWeight;
 
     },
-    fillMatrix: function(lettersWeight){
+    fillMatrix: function(lettersWeight){ // N2
 
         let matrix = {
             '0': ['-', '-', '-', '-'],
@@ -52,7 +59,7 @@ module.exports = {
         let solution = " ABCD";
         for(var row in matrix){
             matrix[row].forEach(function(cell, index){
-                matrix[row][index] = compareLetters(lettersWeight, row, index.toString());
+                matrix[row][index] = compareLetters(lettersWeight, Number(row), index);
             });
             solution += '\n' + indexToLetters[row] + matrix[row].join("");
         }
